c8b89c59b3456fd8f150c88280019f3a054d4d16
Evander Owusu <owu.evander@gmail.com>
2021-12-08 01:22:27 +0000

    add delete button and methoud to delete urls

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +10,10 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {
+  return Math.random().toString(20).substr(2, 6);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,12 +32,33 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let randomStr = generateRandomString();
+  urlDatabase[randomStr] = req.params.longURL;
+  res.render(`/urls/${randomStr}`);
+});
+
+app.post("/urls/:shortUrl/delete", (req, res) => {
+  delete urlDatabase[HTMLTableRowElement.params.shortURL];
+  res.redirect("urls");
 });
 
 app.listen(PORT, () => {
