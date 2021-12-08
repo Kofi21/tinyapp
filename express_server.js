19e0ed3aa2a4cc556282fe7fa29a37ba19bab52a
Evander Owusu <owu.evander@gmail.com>
2021-12-08 01:55:46 +0000

    add cookie functionality and username to website

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -15,8 +17,15 @@ function generateRandomString() {
   return Math.random().toString(20).substr(2, 6);
 }
 
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  // Cookies that have not been signed
+  console.log("Cookies: ", req.cookies);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Welcome to Tinyapp");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -28,18 +37,31 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  //What would happen if a client requests a non-existent shortURL?
+  if (!urlDatabase[shortURL]) {
+    res.send("<html><body>Error</body></html>\n");
+    return;
+  }
   const templateVars = {
-    shortURL: req.params.shortURL,
+    shortURL: shortURL,
     longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -50,24 +72,43 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  let randomStr = generateRandomString();
-  urlDatabase[randomStr] = req.params.longURL;
-  res.render(`/urls/${randomStr}`);
+  console.log(req.body); // Log the POST request body to the console
+  let newID = generateRandomString();
+  urlDatabase[newID] = req.body.longURL;
+
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.redirect(`/urls/${newID}`, templateVars); // Respond redirect
 });
 
-app.post("/urls/:shortUrl/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("urls");
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`); // Respond redirect to index page
 });
 
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   urlDatabase[id] = req.body.longURL;
 
-  res.redirect(`/urls`); // Respond redirect
+  res.redirect(`/urls`); // Respond redirect to index page
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+
+  res.cookie("username", username);
+
+  res.redirect(`/urls`); // Respond redirect to index page
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+
+  res.redirect(`/urls`); // Respond redirect to index page
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tinyapp is listening on port ${PORT}.`);
 });
